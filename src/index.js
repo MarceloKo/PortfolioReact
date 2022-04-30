2f448a22339dd29d4281e36524e2d56013c668f0
MarceloKo <marceloakohlhase@gmail.com>
2022-04-29 22:48:57 -0400

    Middleware authentication

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/routes');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const app = express();
+
+app.use(bodyParser.json());
+app.use( bodyParser.urlencoded({extended:false}))
+app.use(routes)
+app.get('/', (req, res) => {
+    res.json({message:"Hello World"});
+})
+
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@apicluster.f1hea.mongodb.net/Dashboard?retryWrites=true&w=majority`)
+    .then(()=>{
+        app.listen(process.env.PORT)    
+    })
+    .catch(err => console.log(err))
