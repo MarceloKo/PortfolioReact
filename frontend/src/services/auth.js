f5f267f156f4bc52aa44eb0d8aaed9e8fd37b130
MarceloKo <marceloakohlhase@gmail.com>
2022-05-20 11:51:25 -0400

    Fix - token

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,14 +1,11 @@
-import useToken from "../store/storeToken";
-
 export const TOKEN_KEY = "token";
 export const ID_USUARIO= "id_user";
 export const NOME_USUARIO= "name_user";
-const token = useToken((state)=>{state.token})
+
 export const login = (token,id, name) => {
     localStorage.setItem(TOKEN_KEY, token);
     localStorage.setItem(NOME_USUARIO, name);
-    const setToken = useToken((state)=>{state.setToken})
-    setToken(token)
+
 }
 
 export const logout = () => {
@@ -17,6 +14,9 @@ export const logout = () => {
     localStorage.clear(NOME_USUARIO);
 }
 
-export const headers = 
-  {headers: {authorization: "Bearer " + localStorage.getItem(token)}}
+export const headers = ()=>
+  {
+    const token = localStorage.getItem("token")
+      return {headers: {authorization: `Bearer ${token}`}}
+    }
 
