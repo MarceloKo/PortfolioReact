146d32270bbf1a6662960a33c25f42c66dad3e6c
MarceloKo <marceloakohlhase@gmail.com>
2022-06-01 14:18:47 -0400

    Fix  error auth

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -1,32 +1,37 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json');
 const User = require('../models/User');
-module.exports = async(req,res,next)=>{
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).json({error:"Token não informado!"})
-    }
-    const parts = authHeader.split(' ');
-    if(!parts.length === 2){
-        return res.status(401).json({error:"Token mal formatado!"})
-    }
-    const [scheme,token] = parts;
-    if(!/^Bearer$/i.test(scheme)){
-        return res.status(401).json({error:"Token mal formatado!"})
-    }
-    
-    jwt.verify(token,authConfig.secret,async(err,decoded)=>{
-        if(err){
-            return res.status(401).json({error:"Token inválido!"})
+module.exports = async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.status(401).json({ error: "Token não informado!" })
         }
+        const parts = authHeader.split(' ');
+        if (!parts.length === 2) {
+            return res.status(401).json({ error: "Token mal formatado!" })
+        }
+        const [scheme, token] = parts;
+        if (!/^Bearer$/i.test(scheme)) {
+            return res.status(401).json({ error: "Token mal formatado!" })
+        }
+
+        jwt.verify(token, authConfig.secret, async(err, decoded) => {
+            if (err) {
+                return res.status(401).json({ error: "Token inválido!" })
+            }else{
+                req.userId = decoded.id;
+                const response = await User.findById(req.userId);
         
-        req.userId = decoded.id;
-        
-    })
-    const response = await User.findById(req.userId);
-    if(!response){
-        return res.status(401).json({error:"Usuário não encontrado!"})
+                if (!response) {
+                    return res.status(401).json({ error: "Usuário não encontrado!" })
+                }
+                return next();
+            }
+        }).catch(err => {console.log(err)})
+ 
+    } catch {
+        return res.status(401).json({ error: "Token inválido!" })
     }
-    return next();
-    
+
 }
